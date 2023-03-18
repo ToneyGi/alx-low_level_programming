2cb3d8934a7d5a4ae603b0d6297a8c5f47804265
Tony Maina Hub <minortione@gmail.com>
2023-03-18 05:52:22 -0700

    werror

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main fanction
+ *
+ * Return: 0 when successfull
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
+
